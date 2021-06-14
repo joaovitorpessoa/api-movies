a9108e4f55c515293a57ba4aa8ac3e013eee1ad1
João Vitor Pessoa <joaovitorpessoa81@gmail.com>
2021-06-14 15:51:56 -0300

    add: process.env.PORT

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
 import { app } from "./app"; // Import da API
 
-app.listen(3333, () => console.log("Server is running on port 3333! 🚀")); // Roda a aplicação na porta informada pela variável de ambiente
+app.listen(process.env.PORT || 3333, () =>
+  console.log("Server is running! 🚀")
+); // Roda a aplicação na porta informada pela variável de ambiente
